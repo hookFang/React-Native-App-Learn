84bacb663ab66d2b9abd8d2711a9fbb7fd4fd1d3
Edwin Christie <edwinchristie100@gmail.com>
2021-01-08 20:00:40 -0500

    Project Added, with a welcome message in loading page

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <Text style={{ fontSize: 50 }}>Welcome</Text>
+    </View>
+  );
+}
